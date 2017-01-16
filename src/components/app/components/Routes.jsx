11984511524b6f05b54e5c8d7d0ccf77968d56c9
ismay <ismay@ismaywolff.nl>
2017-01-16 18:25:42 +0100

    Update styles

--- src/components/app/components/Routes.jsx
+++ src/components/app/components/Routes.jsx
@@ -1,20 +1,26 @@
 import React from 'react'
 import Match from 'react-router/Match'
 import Miss from 'react-router/Miss'
+import styled from 'styled-components'
 import { About } from '../../about'
 import { Header } from '../../header'
 import { Missing } from '../../missing'
 import { WorkDetailContainer, WorkListContainer } from '../../works'
 
+const Container = styled.div`
+  margin: 0 auto;
+  max-width: 40rem;
+`
+
 function Routes() {
   return (
-    <div>
+    <Container>
       <Header />
       <Match exactly pattern="/" component={WorkListContainer} />
       <Match pattern="/work/:id" component={WorkDetailContainer} />
       <Match pattern="/about" component={About} />
       <Miss component={Missing} />
-    </div>
+    </Container>
   )
 }
 
--- src/components/header/components/Header.jsx
+++ src/components/header/components/Header.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Navigation } from '../../navigation'
+
+const Center = styled.div`
+  align-items: center;
+  display: flex;
+`
+
+const PushRight = styled.div`
+  margin-left: auto;
+`
 
 function Header() {
   return (
-    <header>
+    <Center>
       <h1>Ismay Wolff</h1>
-      <Navigation />
-    </header>
+      <PushRight>
+        <Navigation />
+      </PushRight>
+    </Center>
   )
 }
 
